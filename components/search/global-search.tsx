81ebd59008caf682af8453822d13a290acadf9ad
Vanshaj Poonia <vanshajtheunique@gmail.com>
2026-07-14 21:22:06 +0530

    Widen global search on larger screens

--- components/search/global-search.tsx
+++ components/search/global-search.tsx
@@ -98,7 +98,7 @@ export default function GlobalSearch({ isAdmin }: GlobalSearchProps) {
   }
 
   return (
-    <div ref={containerRef} className="relative w-full max-w-sm">
+    <div ref={containerRef} className="relative w-full max-w-sm sm:max-w-xl">
       <div className="relative">
         <Search className="absolute left-2.5 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
         <Input
